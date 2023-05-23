c603f2c74ed351dd114a81bf5ffe08e48986043f
Swarga-codes <sd3526@srmist.edu.in>
2023-05-23 22:37:08 +0530

    added autoplay and fixed css

--- src/Components/Watch/Watch.js
+++ src/Components/Watch/Watch.js
@@ -8,14 +8,13 @@ import ModeCommentIcon from '@mui/icons-material/ModeComment';
 function Watch() {
    const location=useLocation()
    const data=location.state.data;
-
-    useEffect(()=>{
+    useEffect(()=>{    
 window.scrollTo(0,0)
     },[])
   return (
     <Box className={WatchStyles.Watch}>
     <Box className={WatchStyles.player_detail}>
-    <ReactPlayer url={data.submission.mediaUrl} controls width="100%" height="25rem"/>
+    <ReactPlayer url={data.submission.mediaUrl} controls width="100%" height="25rem" playing={true}/>
     <Box className={WatchStyles.video_details}>
     <Typography fontSize='larger' fontWeight='600' mt='1rem'>{data.submission.title}</Typography>
     <Box display='flex' mt="1rem" className={WatchStyles.channel_stats}>
